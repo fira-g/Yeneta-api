c216a890964164f0afc4d0dd4f492d60a90a9164
fira-g <firaolgula@gmail.com>
2025-04-29 14:55:22 +0300

    upload event image

--- src/routes/events.route.js
+++ src/routes/events.route.js
@@ -6,12 +6,13 @@ import {
   rsvp,
 } from "../controllers/events.controller.js";
 import { isPremium, protectRout } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getEvents);
 router.get("/myEvents", protectRout, getMyEvents);
-router.post("/", protectRout, isPremium, createEvent);
+router.post("/", protectRout, isPremium, upload.single("image"), createEvent);
 router.post("/:eventId/rsvp", protectRout, rsvp);
 
 export default router;
